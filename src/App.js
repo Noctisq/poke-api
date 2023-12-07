b93edbfc41f1d19b038270d2630b89197b99b4aa
unknown <noctisumire@gmail.com>
2023-12-06 18:35:01 -0600

    feat(pokemonList): added more stateful variables to handle pagination

--- src/App.js
+++ src/App.js
@@ -1,36 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
-import { PokemonCard } from './components/PokemonCard'
+import PokemonCard from './components/PokemonCard'
+import Pagination  from './components/Pagination';
+import Loader from './components/Loader';
+
 function App() {
 
   const [pokemonList, setPokemonList] = useState([]);
-
+  const [currentPage, setCurrentPage] = useState("https://pokeapi.co/api/v2/pokemon/?limit=30");
+  const [nextPage, setnextPage] = useState(null);
+  const [previousPage, setpreviousPage] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
-
-    fetch("https://pokeapi.co/api/v2/pokemon/?limit=60").then(data => {
-      return data.json()
+    setIsLoading(true);
+    fetch(currentPage).then(data => {
+      return data.json();
     }).then(response => {
-      console.log(response.results)
       setPokemonList(response.results);
-
+      setIsLoading(false);
+      setnextPage(response.next)
+      setpreviousPage(response.previous);
     }).catch(err => {
       console.log(err);
     })
-  }, [])
+    return () => setIsLoading(true)
+  }, [currentPage]);
+
+
 
   return (
     <div className="App">
+      {isLoading ? (
+        <Loader/>
+      ) : (
+        <>
+          <section className='pokemon-data'>
+            <ul className='pokemon-list'>
+              {pokemonList.length > 0 &&
+                pokemonList.map((pokemon, index) => (
+                  <PokemonCard key={index} data={pokemon} />
+                ))}
+            </ul>
+          </section>
 
-      <section className='pokemon-list'>
-        {pokemonList.length > 0 &&
-          pokemonList.map(value => <PokemonCard data={value} />)
-        }
-      </section>
-
-      <footer>
-        ahhh
-      </footer>
+          <footer>
+            {pokemonList && (
+              <Pagination
+                nextPage={nextPage}
+                previousPage={previousPage}
+                setCurrentPage={setCurrentPage}
+              />
+            )}
+          </footer>
+        </>
+      )}
     </div>
   );
 }
